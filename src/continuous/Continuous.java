e9d0f5b79ba5eda5fb32deed394c029c88054815
Samuel Waggoner <samuel.waggoner@gmail.com>
2013-09-09 22:53:20 -0500

    Frame stuff.

--- src/continuous/Continuous.java
+++ src/continuous/Continuous.java
@@ -1,7 +1,30 @@
 package continuous;
 
-public class Continuous {
-	public static void main(String[] args) {
+import java.awt.Canvas;
+import java.awt.Graphics;
 
-	}
+import javax.swing.JFrame;
+
+public class Continuous extends Canvas {
+    public static void main(String[] args) {
+        new Frame(new Canvas()).render();
+    }
+    
+    public void paint(Graphics g) {
+        
+    }
+}
+
+@SuppressWarnings("serial")
+class Frame extends JFrame {
+    Canvas canvas;
+    
+    public Frame(Canvas canvas) {
+        this.canvas = canvas;
+    }
+    
+    public void render() {
+        Graphics g = canvas.getGraphics();
+        canvas.paint(g);
+    }
 }
